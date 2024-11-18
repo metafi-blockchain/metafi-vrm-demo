5181d5010780113daf5ee7095bb7c0578299581a
Do Danh Hau <dohau2000hp@gmail.com>
2024-11-18 13:49:54 +0700

    update dropdownbbox vrma

--- src/contants.ts
+++ src/contants.ts
@@ -1,11 +1,22 @@
 export const listVRMS = [
-    {
-        value: '/vrmas/VRMA_01.vrma',
-        name: 'VRMA_01',
-    },
-    {
-        value: '/vrmas/VRMA_02.vrma',
-        name: 'VRMA_02',
-    },
-
-]
+  {
+    value: "/vrmas/VRMA_01.vrma",
+    name: "VRMA_01",
+  },
+  {
+    value: "/vrmas/VRMA_02.vrma",
+    name: "VRMA_02",
+  },
+  {
+    value: "/vrmas/VRMA_03.vrma",
+    name: "VRMA_03",
+  },
+  {
+    value: "/vrmas/VRMA_04.vrma",
+    name: "VRMA_04",
+  },
+  {
+    value: "/vrmas/VRMA_05.vrma",
+    name: "VRMA_05",
+  },
+];
--- src/vrm.tsx
+++ src/vrm.tsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useLayoutEffect, useState, useRef } from 'react';
-
+import React, { useEffect, useLayoutEffect, useState, useRef } from "react";
 
 // for vrm
-import * as THREE from 'three';
-import { VRM } from '@pixiv/three-vrm';
-import { useVRM } from './lib/useVRM';
-import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import { PerspectiveCamera } from '@react-three/drei';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { createVRMAnimationClip, VRMAnimation, VRMAnimationLoaderPlugin } from '@pixiv/three-vrm-animation';
-import {listVRMS} from '../src/contants';
+import * as THREE from "three";
+import { VRM } from "@pixiv/three-vrm";
+import { useVRM } from "./lib/useVRM";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import {
+  createVRMAnimationClip,
+  VRMAnimation,
+  VRMAnimationLoaderPlugin,
+} from "@pixiv/three-vrm-animation";
+import { listVRMS } from "../src/contants";
 export default function Model() {
-
-
   const rootRef = useRef<HTMLDivElement | null>(null);
   const { vrm } = useVRM();
-
-
+  const [selectedVRMA, setSelectedVRMA] = useState<string>(listVRMS[0].value);
 
   useLayoutEffect(() => {
     const root = rootRef.current;
@@ -28,33 +28,48 @@ export default function Model() {
     };
     resizeCanvas();
 
-    window.addEventListener('resize', resizeCanvas);
+    window.addEventListener("resize", resizeCanvas);
     return () => {
-      window.removeEventListener('resize', resizeCanvas);
+      window.removeEventListener("resize", resizeCanvas);
     };
   }, []);
 
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedVRMA(event.target.value);
+  };
+
   return (
     <div ref={rootRef}>
+      <div>
+        <label htmlFor="">chọn VRMA: </label>
+        <select
+          name="select-vrma"
+          id="select-vrma"
+          value={selectedVRMA}
+          onChange={handleChange}
+        >
+          {listVRMS.map((fileVRMA, index) => (
+            <option key={index} value={fileVRMA.value}>
+              {fileVRMA.name}
+            </option>
+          ))}
+        </select>
+      </div>
       {vrm == undefined ? (
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-          </div>
-
+        <div style={{ display: "flex", justifyContent: "center" }}></div>
       ) : (
         <Canvas flat>
           <PerspectiveCamera makeDefault position={[-0.12, 1, 4]} />
-          <Avatar vrm={vrm} />
+          <Avatar vrm={vrm} selectedVRMA={selectedVRMA} />
           <directionalLight />
         </Canvas>
       )}
-
-
     </div>
   );
 }
 
 /** VRMアバターを表示するコンポーネント */
-const Avatar = ({ vrm }: { vrm: VRM }) => {
+const Avatar = ({ vrm, selectedVRMA }: { vrm: VRM; selectedVRMA: string }) => {
   const mixer = useRef<THREE.AnimationMixer>();
   const action = useRef<THREE.AnimationAction>();
   const [show, setShow] = useState(false);
@@ -69,21 +84,25 @@ const Avatar = ({ vrm }: { vrm: VRM }) => {
     }
   });
 
-  const vrmaContainer = useLoader(GLTFLoader, listVRMS[0].value, (loader) => {
+  const vrmaContainer = useLoader(GLTFLoader, selectedVRMA, (loader) => {
     loader.register((parser) => {
       return new VRMAnimationLoaderPlugin(parser);
     });
   });
 
-  debugger
-  const vrma = (vrmaContainer.userData.vrmAnimations?.[0] ?? undefined) as VRMAnimation | undefined;
+  // debugger;
+  const vrma = (vrmaContainer.userData.vrmAnimations?.[0] ?? undefined) as
+    | VRMAnimation
+    | undefined;
 
   useEffect(() => {
     const loadAnimation = async () => {
       if (!vrm) return;
       if (!vrma) return;
 
-      const mixerTmp: THREE.AnimationMixer = new THREE.AnimationMixer(vrm.scene);
+      const mixerTmp: THREE.AnimationMixer = new THREE.AnimationMixer(
+        vrm.scene
+      );
       mixer.current = mixerTmp;
 
       const clip = createVRMAnimationClip(vrma, vrm);
@@ -97,4 +116,3 @@ const Avatar = ({ vrm }: { vrm: VRM }) => {
 
   return show ? <primitive object={vrm.scene}></primitive> : <></>;
 };
-
